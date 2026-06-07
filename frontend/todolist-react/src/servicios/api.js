885c9530de5e5515bdc90eaa61155b5ef5f85464
Adalid Josue Sarmiento <sarmiento.josue97@gmail.com>
2026-06-07 15:15:28 -0400

    error n2 solucionado

--- frontend/todolist-react/src/servicios/api.js
+++ frontend/todolist-react/src/servicios/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { obtenerToken, eliminarToken } from './auth';
 
 const cache_etags = {};
+const cache_datos = {};
 
 const api = axios.create({
     baseURL: '',
@@ -23,12 +24,18 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
     (response) => {
         const etag = response.headers['etag'];
-        if(etag) cache_etags[response.config.url] = etag;
+        if(etag) {cache_etags[response.config.url] = etag;
+            cache_datos[response.config.url] = response.data; 
+        }
         return response;
     },
 
     (error) => {
         if(error.response?.status === 304){
+            const datos_previos = cache_datos[error.config?.url];
+            if(datos_previos){
+                return Promise.resolve({ ...error.response, data: datos_previos });
+            }
             return Promise.resolve(error.response);
         }
 
